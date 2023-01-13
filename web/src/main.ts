cd09dd780301b842a748a914d776c6be387b9239
kubeops <kubeops@163.com>
2023-01-13 14:15:53 +0800

    reinstall web

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
