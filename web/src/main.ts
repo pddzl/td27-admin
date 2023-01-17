b57f5ac75e8decbc4579b08472914b623f5d217b
kubeops <kubeops@163.com>
2023-01-17 09:56:36 +0800

    去掉main.css

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
+import store from "@/store"
 import router from './router'
-
-import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(store)
 app.use(router)
-
 app.mount('#app')
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,23 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { type RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
+
+const Layout = () => import("@/layout/index.vue")
+
+// 常驻路由
+export const constantRoutes: RouteRecordRaw[] = []
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHashHistory(),
+  routes: constantRoutes
 })
 
 export default router
