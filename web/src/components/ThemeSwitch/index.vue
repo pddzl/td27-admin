748918f7b21b0ea0527ab6fe9375c6c5fbee5b4d
kubeops <kubeops@163.com>
2023-01-18 15:30:22 +0800

    ThemeSwitch

---
+++ web/src/components/ThemeSwitch/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-dropdown trigger="click" @command="handleSetTheme">
+    <div>
+      <el-tooltip effect="dark" content="主题模式" placement="bottom">
+        <el-icon :size="20">
+          <MagicStick />
+        </el-icon>
+      </el-tooltip>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item
+          v-for="(theme, index) in themeList"
+          :key="index"
+          :disabled="activeThemeName === theme.name"
+          :command="theme.name"
+        >
+          <span>{{ theme.title }}</span>
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script lang="ts" setup>
+import { type ThemeName, useTheme } from "@/hooks/useTheme"
+import { MagicStick } from "@element-plus/icons-vue"
+
+const { themeList, activeThemeName, setTheme } = useTheme()
+
+const handleSetTheme = (name: ThemeName) => {
+  setTheme(name)
+}
+</script>
+
