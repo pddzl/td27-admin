94af9ef58d96fa24e3fb769cf5e6e01f55d17ad8
kubeops <kubeops@163.com>
2023-03-01 16:18:46 +0800

    添加表单校验

--- web/src/views/personal/index.vue
+++ web/src/views/personal/index.vue
@@ -4,11 +4,11 @@
       <el-tabs>
         <el-tab-pane label="基本信息">
           <div class="form-container">
-            <el-form label-width="120px" :model="userInfoForm">
+            <el-form label-width="120px" :model="userInfoForm" :rules="userInfoRule" ref="userInfoFormRef">
               <el-form-item label="创建时间">
                 <el-input style="width: 400px" v-model="userInfoForm.createdAt" disabled />
               </el-form-item>
-              <el-form-item label="用户名" required>
+              <el-form-item label="用户名" prop="username" required>
                 <el-input style="width: 400px" v-model="userInfoForm.username" />
               </el-form-item>
               <el-form-item label="角色">
@@ -21,7 +21,9 @@
                 <el-input style="width: 400px" v-model="userInfoForm.email" />
               </el-form-item>
               <el-form-item style="margin-top: 40px">
-                <el-button type="primary" style="margin-right: 20px" @click="handleEditUser">更新</el-button>
+                <el-button type="primary" style="margin-right: 20px" @click="handleEditUser(userInfoFormRef)"
+                  >更新</el-button
+                >
                 <el-button type="primary" plain @click="toDefault">关闭</el-button>
               </el-form-item>
             </el-form>
@@ -52,9 +54,9 @@
 </template>
 
 <script lang="ts" setup>
-import { reactive } from "vue"
+import { ref, reactive } from "vue"
 import { useRouter } from "vue-router"
-import { ElMessage } from "element-plus"
+import { type FormInstance, type FormRules, ElMessage } from "element-plus"
 import { formatDateTime } from "@/utils/index"
 import { useUserStore } from "@/store/modules/user"
 import { editUserApi } from "@/api/system/user"
@@ -66,6 +68,7 @@ const toDefault = () => {
   router.push("/")
 }
 
+const userInfoFormRef = ref<FormInstance>()
 const userInfoForm = reactive({
   id: 0,
   createdAt: "",
@@ -75,6 +78,8 @@ const userInfoForm = reactive({
   role: "",
   roleId: 0
 })
+
+const userInfoRule: FormRules = reactive({ username: [{ required: true, trigger: "blur", message: "请填写用户名" }] })
 
 const getCache = () => {
   userInfoForm.id = userStore.userInfo.id
@@ -87,21 +92,28 @@ const getCache = () => {
 }
 getCache()
 
-const handleEditUser = () => {
-  editUserApi({
-    id: userInfoForm.id,
-    username: userInfoForm.username,
-    phone: userInfoForm.phone,
-    email: userInfoForm.email,
-    active: true,
-    roleId: userInfoForm.roleId
+const handleEditUser = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      editUserApi({
+        id: userInfoForm.id,
+        username: userInfoForm.username,
+        phone: userInfoForm.phone,
+        email: userInfoForm.email,
+        active: true,
+        roleId: userInfoForm.roleId
+      })
+        .then((res) => {
+          if (res.code === 0) {
+            ElMessage({ type: "success", message: res.msg })
+          }
+          // 刷新userinfo store
+          userStore.getInfo()
+        })
+        .catch(() => {})
+    }
   })
-    .then((res) => {
-      if (res.code === 0) {
-        ElMessage({ type: "success", message: res.msg })
-      }
-    })
-    .catch(() => {})
 }
 </script>
 
