4ae9abd1025801456b4a3ce84738ceebb4d805fb
kubeops <kubeops@163.com>
2023-03-15 17:26:57 +0800

    优化docker-compose

--- server/wait-for.sh
+++ server/wait-for.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-until nc -w2 -t mysql 3306 && nc -w2 -t redis 6379
+until nc -w2 mysql 3306 && nc -w2 redis 6379
 do
   echo "mysql or redis is unavailable - sleeping" >&2
   sleep 1
