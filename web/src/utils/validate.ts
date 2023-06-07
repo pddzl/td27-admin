eff524bf01cae375ad0f1d366d07682b6fd25ffa
pddzl <pddzl5@163.com>
2023-06-07 15:47:43 +0800

    test

--- web/src/utils/validate.ts
+++ web/src/utils/validate.ts
@@ -79,6 +79,6 @@ export const isIPv4 = (ip: string) => {
 /** 判断是否为车牌（兼容新能源车牌） */
 export const isLicensePlate = (str: string) => {
   const reg =
-    /^[京1津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-HJ-NP-Z][A-HJ-NP-Z0-9]{4,5}[A-HJ-NP-Z0-9挂学警港澳]$/
+    /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-HJ-NP-Z][A-HJ-NP-Z0-9]{4,5}[A-HJ-NP-Z0-9挂学警港澳]$/
   return reg.test(str)
 }
