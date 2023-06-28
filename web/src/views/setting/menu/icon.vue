7911791c0433b9b5dd47517ee9973392bae60c18
pddzl <kubeops@163.com>
2023-06-28 11:19:40 +0800

    上传svg&修改添加菜单表单图标样式

--- web/src/views/setting/menu/icon.vue
+++ web/src/views/setting/menu/icon.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <span class="td27-icon" style="position: absolute; z-index: 9999; padding: 3px 10px 0">
-      <svg-icon :name="metaData.icon" />
+    <span class="td27-icon" style="position: absolute; z-index: 9999; padding: 3px 15px 0; ">
+      <SvgIcon :name="metaData.icon" />
     </span>
     <el-select
       v-model="metaData.icon"
@@ -18,8 +18,8 @@
         :label="item.key"
         :value="item.key"
       >
-        <span class="td27-icon">
-          <svg-icon :name="item.label" />
+        <span class="td27-icon" style=" padding: 3px 0 0;">
+          <SvgIcon :name="item.label" />
         </span>
         <span style="text-align: left">{{ item.key }}</span>
       </el-option>
@@ -41,9 +41,16 @@ const props = defineProps({
 
 const options = reactive([
   { key: "dashboard", label: "dashboard" },
+  { key: "setting", label: "setting" },
   { key: "lock", label: "lock" },
   { key: "menu", label: "menu" },
-  { key: "bug", label: "bug" }
+  { key: "bug", label: "bug" },
+  { key: "network", label: "network" },
+  { key: "plus", label: "plus" },
+  { key: "load", label: "load" },
+  { key: "config", label: "config" },
+  { key: "link", label: "link" },
+  { key: "access", label: "access" }
 ])
 
 const metaData = ref(props.meta)
@@ -51,10 +58,18 @@ const metaData = ref(props.meta)
 
 <style>
 .td27-icon {
+  color: rgb(132, 146, 166);
+  font-size: 14px;
   margin-right: 10px;
 }
 
 .td27-select .el-input__inner {
-  padding: 0 20px !important;
+  padding: 0 30px !important
+}
+
+.select__option_item {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 }
 </style>
