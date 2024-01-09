c0c2eb79be929e5e16e3ae0def75f807b35589cd
pddzl <kubeops@163.com>
2024-01-09 11:23:51 +0800

    登录页面去掉路由错误提示消息弹框

--- web/src/permission.ts
+++ web/src/permission.ts
@@ -42,7 +42,7 @@ router.beforeEach(async (to, _from, next) => {
           // 过程中发生任何错误，都直接重置 Token，并重定向到登录页面
           userStore.resetToken()
           console.log(err)
-          ElMessage.error(err.message || "路由守卫过程发生错误")
+          // ElMessage.error(err.message || "路由守卫过程发生错误")
           next("/login")
           NProgress.done()
         }
