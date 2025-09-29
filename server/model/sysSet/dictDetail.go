9e003402e39282917d031d8e8fba060875e2e64f
pddzl <td27fish@163.com>
2025-09-29 23:14:59 +0800

    dictionary model: set value field uniqueness

--- server/model/sysSet/dictDetail.go
+++ server/model/sysSet/dictDetail.go
@@ -5,7 +5,7 @@ import "server/global"
 type DictDetailModel struct {
 	global.TD27_MODEL
 	Label       string             `json:"label" gorm:"column:label" binding:"required"`
-	Value       string             `json:"value" gorm:"column:value" binding:"required"`
+	Value       string             `json:"value" gorm:"column:value;unique" binding:"required"`
 	Sort        int                `json:"sort" gorm:"column:sort"`
 	DictModelID int                `json:"dictId" gorm:"column:dict_id" binding:"required"`
 	ParentID    *int               `json:"parentId" gorm:"column:parent_id"` // new
