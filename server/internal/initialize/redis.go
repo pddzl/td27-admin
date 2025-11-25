df831ebed443c33322c66cbf0ab4233657cfa5ec
pddzl <td27fish@163.com>
2025-11-25 22:29:19 +0800

    perf: add timeout in redis initialization

--- server/internal/initialize/redis.go
+++ server/internal/initialize/redis.go
@@ -3,25 +3,31 @@ package initialize
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
-	
+
 	"server/internal/global"
 )
 
-func Redis() {
+func Redis() *redis.Client {
 	redisCfg := global.TD27_CONFIG.Redis
 	client := redis.NewClient(&redis.Options{
 		Addr: fmt.Sprintf("%s:%d", global.TD27_CONFIG.Redis.Host, global.TD27_CONFIG.Redis.Port),
 		//Password: redisCfg.Password, // no password set
 		DB: redisCfg.DB, // use default DB
 	})
-	pong, err := client.Ping(context.Background()).Result()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	pong, err := client.Ping(ctx).Result()
 	if err != nil {
 		global.TD27_LOG.Error("redis connect ping failed, err:", zap.Error(err))
 	} else {
 		global.TD27_LOG.Info("redis connect ping response:", zap.String("pong", pong))
-		global.TD27_REDIS = client
 	}
+
+	return client
 }
