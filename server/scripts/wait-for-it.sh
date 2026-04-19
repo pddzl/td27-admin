09c1ac204a5bb803cf9798297d8bc27227918e2c
pddzl <td27fish@163.com>
2026-04-20 01:25:27 +0800

    perf: optimize docker compose

--- server/scripts/wait-for-it.sh
+++ server/scripts/wait-for-it.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-until nc -w2 mysql 3306 && nc -w2 redis 6379
+until nc -w2 pgsql 5432
 do
-  echo "mysql or redis is unavailable - sleeping" >&2
+  echo "pgsql is unavailable - sleeping" >&2
   sleep 1
 done
 
